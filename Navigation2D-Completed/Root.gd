e3ed9680459d8ffd7593ca3a6672d25736787b79
Davide Pesce <davidepesce1980@gmail.com>
2019-11-19 17:11:26 +0100

    Fixes

--- Navigation2D-Completed/Root.gd
+++ Navigation2D-Completed/Root.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			var path = $Navigation2D.get_simple_path($Player.global_position, event.global_position)
+			var path = $Navigation2D.get_simple_path($Player.position, event.position)
 			$Line2D.points = path
 			$Player.path = path
 
